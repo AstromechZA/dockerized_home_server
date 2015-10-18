99b876e16638b7688c58a6dc36a614dcb4c7f442
ben meier <benmeier@fastmail.com>
2015-10-18 10:36:00 +0200

    Added script to build all, tweaked docker files to make sure they get killed properly

---
+++ build_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+echo "Building Docker images"
+
+if [ ! -d ./graphing -o ! -d ./samba -o ! -d ./torrents ]; then
+    echo "Please execute from the correct directory"
+    exit 1
+fi
+
+docker build -t usvr_graphing ./graphing
+docker build -t usvr_samba ./samba
+docker build -t usvr_torrents ./torrents
+
+echo "DONE"
