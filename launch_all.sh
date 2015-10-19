2c897c9f18d770c28f1e33de715cd71259650840
ben meier <benmeier@fastmail.com>
2015-10-19 22:24:48 +0200

    found new technique for linking diamond to host os

--- launch_all.sh
+++ launch_all.sh
@@ -14,22 +14,24 @@ TRANSMISSION_DOWNLOADS_PATH=/tmp/spublic/torrents
 GRAPHITE_STORAGE=/tmp/whispers
 
 # Web ports
+CARBON_PORT=2003
 GRAFANA_PORT=3000
 TRANSMISSION_PORT=9091
 
 echo "launching containers"
 
 docker run -d \
+        -p $CARBON_PORT:2003 \
         -p $GRAFANA_PORT:3000 \
         -v $GRAPHITE_STORAGE:/opt/graphite/storage/whisper \
         --name usvr_g \
         usvr_graphing
 
 docker run -d \
-        --link usvr_g:usvr_g \
-        -v /proc:/host_proc \
-        -e GRAPHITE_HOST=@USVR_G_PORT_2003_TCP_ADDR \
-        -e GRAPHITE_PORT=@USVR_G_PORT_2003_TCP_PORT \
+        -v /:/host \
+        --net=host \
+        -e GRAPHITE_HOST=127.0.0.1 \
+        -e GRAPHITE_PORT=$CARBON_PORT \
         -e HOST_HOSTNAME=$(hostname) \
         --name usvr_d \
         usvr_diamond
