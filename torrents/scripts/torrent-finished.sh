833695aedd8564807a7569cfd22e5984caafb167
Ben Meier <benmeier@fastmail.com>
2016-11-07 17:50:01 +0200

    got rid of a bunch of errors in the log

---
+++ torrents/scripts/torrent-finished.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Executing $0 due to finished torrent: $TR_TORRENT_NAME.."
+echo "Envars:"
+echo "TR_APP_VERSION:    $TR_APP_VERSION"
+echo "TR_TIME_LOCALTIME: $TR_TIME_LOCALTIME"
+echo "TR_TORRENT_DIR:    $TR_TORRENT_DIR"
+echo "TR_TORRENT_HASH:   $TR_TORRENT_HASH"
+echo "TR_TORRENT_ID:     $TR_TORRENT_ID"
+echo "TR_TORRENT_NAME:   $TR_TORRENT_NAME"
+
+CHOWN_USER="<changeme>"
+
+echo "Done."
