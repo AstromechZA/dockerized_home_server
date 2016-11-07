6cc4d9f9fad41d19e5710e6c0b78921410c8690f
Ben Meier <benmeier@fastmail.com>
2016-11-07 19:39:21 +0200

    added log stuff to script

--- torrents/scripts/torrent-finished.sh
+++ torrents/scripts/torrent-finished.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+exec > /var/log/transmission/finished-scripts.log
+
 echo "Executing $0 due to finished torrent: $TR_TORRENT_NAME.."
 echo "Envars:"
 echo "TR_APP_VERSION:    $TR_APP_VERSION"
@@ -8,7 +10,5 @@ echo "TR_TORRENT_DIR:    $TR_TORRENT_DIR"
 echo "TR_TORRENT_HASH:   $TR_TORRENT_HASH"
 echo "TR_TORRENT_ID:     $TR_TORRENT_ID"
 echo "TR_TORRENT_NAME:   $TR_TORRENT_NAME"
-
-CHOWN_USER="<changeme>"
 
 echo "Done."
